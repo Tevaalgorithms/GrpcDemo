10c91ac1e95c55c6fb80de775c658171b4386cd0
Tevaalgorithms <Tevaalgorithms@gmail.com>
2023-06-07 00:04:29 -0600

    Adding extra services

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("First Service - greet Proto");
+
+            var input = new HelloRequest { Name = "Teva"};
+            var channel = GrpcChannel.ForAddress("https://localhost:7027");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+
+
+            Console.WriteLine("Second Service - Customer Proto");
+
+            var customerclient = new Customers.CustomersClient(channel);
+
+            var clientRequest = new CustomerLookupModel { UserId = 1 };
+
+            var customer = await customerclient.GetCustomerInfoAsync(clientRequest);
+
+            Console.WriteLine($"{ customer.FirstName} {customer.LastName}");
+
+            Console.WriteLine("New Customer List");
+
+            using (var call = customerclient.GetNewCustomer(new NewCustomerRequest()))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{currentCustomer.FirstName} : {currentCustomer.LastName}: {currentCustomer.EmailAddress}");
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,78 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customers.CustomersBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public CustomersService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            // Simulation of Database lookup
+            if(request.UserId == 1)
+            {
+                output.FirstName = "Teva";
+                output.LastName = "Velu";
+            }
+            else if (request.UserId == 1)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            } 
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomer(
+            NewCustomerRequest request, 
+            IServerStreamWriter<CustomerModel> responseStream, 
+            ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>()
+            { 
+                new CustomerModel
+                {
+                    FirstName = "Teva",
+                    LastName = "Velu",
+                    EmailAddress = "test123@gmail.com",
+                    Age = 101,
+                    IsAlive= true,
+                },
+                 new CustomerModel
+                {
+                    FirstName = "Sue",
+                    LastName = "Strom",
+                    EmailAddress = "sure123@gmail.com",
+                    Age = 31,
+                    IsAlive= true,
+                },
+                  new CustomerModel
+                {
+                    FirstName = "Bibilo",
+                    LastName = "Beggins",
+                    EmailAddress = "bib234@gmail.com",
+                    Age = 51,
+                    IsAlive= false,
+                }
+            };
+
+            foreach(var customer in customers)
+            {
+                // This is just for simulating the delay in getting records
+                await Task.Delay(1000);
+
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
